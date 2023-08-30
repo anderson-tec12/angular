c7c042d34e11cf2e3eacaaa06e7db361a826ffa7
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-08-30 20:15:26 -0300

    Adicionando imagens  e  implemententando interface weather, requisitanto api open map  wether

--- weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { WeatherDatas } from 'src/app/model/interfaces/weatherInterface';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
+  initialCityName = 'Diadema'
+  weatherData!:WeatherDatas
+
+  constructor(
+    private weatherService:WeatherService
+  ){}
+
+  getWeatherDatas(cityName:string):void{
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .subscribe({
+        next: (res) => {
+
+          if(res){
+            this.weatherData = res
+
+          }
+
+        },
+        error: (err) => {
+          console.dir(err)
+        }
+      })
+  }
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName)
+  }
 
 }
