ac84a9096cefc770e1ea6ebe2720b09044e36d95
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-09-04 20:19:01 -0300

    Assitindo formulario de registro

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -13,10 +13,20 @@ export class HomeComponent {
     password:['', Validators.required]
   })
 
+  signupForm = this.formBuilder.group({
+    name:['', Validators.required],
+    email:['', Validators.required],
+    password:['', Validators.required],
+  })
+
   constructor(private formBuilder:FormBuilder){}
 
 
   onSubmitLoginForm(){
     console.log('Dados do formulario de login', this.loginForm.value)
+  }
+
+  onSubmitSignupForm(){
+    console.log('Dados do formulario de signup', this.signupForm.value)
   }
 }
