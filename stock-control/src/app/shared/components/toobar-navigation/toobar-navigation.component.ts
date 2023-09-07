3dbe055c8e7f2c4d608d5c17ce4ee79a57654d81
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-09-07 13:13:14 -0300

    Criação da toobar

---
+++ stock-control/src/app/shared/components/toobar-navigation/toobar-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toobar-navigation',
+  templateUrl: './toobar-navigation.component.html',
+  styleUrls: ['./toobar-navigation.component.scss']
+})
+export class ToobarNavigationComponent {
+
+}
