c3c8f9e99aa369630a1ad31ac72e2b7d9fa67a15
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-09-07 13:25:14 -0300

    Implementando função de logout

--- stock-control/src/app/shared/components/toobar-navigation/toobar-navigation.component.ts
+++ stock-control/src/app/shared/components/toobar-navigation/toobar-navigation.component.ts
@@ -1,4 +1,6 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-toobar-navigation',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./toobar-navigation.component.scss']
 })
 export class ToobarNavigationComponent {
+  constructor(
+    private cookieService:CookieService,
+    private router:Router
+  ){}
+
+
+  handle__logOut():void{
+    this.cookieService.delete('USER_INFO')
+
+    this.router.navigate(['/home'])
+  }
 
 }
