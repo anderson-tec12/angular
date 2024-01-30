61654d16b66a174a0857be82a26b37b8a280f0ca
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-01-30 07:29:23 -0300

    feat: evitando memory leek

--- weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas.interface';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: ['./weather-home.component.scss']
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject()
   initialCityName = 'São Paulo'
   weatherDatas!: WeatherDatas
 
   constructor(private weatherService:WeatherService){}
 
   getWeatherData(cityName:string):void{
-    this.weatherService.getWeatherData(cityName).subscribe({
+    this.weatherService.getWeatherData(cityName).pipe(takeUntil(this.destroy$)).subscribe({
       next:(response) => {
         response && (this.weatherDatas = response)
         console.log(response)
@@ -27,5 +29,10 @@ export class WeatherHomeComponent implements OnInit {
 
   ngOnInit(): void {
       this.getWeatherData(this.initialCityName)
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next()
+      this.destroy$.complete()
   }
 }
