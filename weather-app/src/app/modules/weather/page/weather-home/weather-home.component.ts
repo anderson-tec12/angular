cfa78ce8abe366b607cc795eb137f3272759ab68
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-01-30 07:24:16 -0300

    feat: adição do service e tipagem dos dados

--- weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: ['./weather-home.component.scss']
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'São Paulo'
+  weatherDatas!: WeatherDatas
 
+  constructor(private weatherService:WeatherService){}
+
+  getWeatherData(cityName:string):void{
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next:(response) => {
+        response && (this.weatherDatas = response)
+        console.log(response)
+      },
+      error:(err) => {
+
+      }
+    })
+  }
+
+  ngOnInit(): void {
+      this.getWeatherData(this.initialCityName)
+  }
 }
