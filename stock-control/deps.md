dd5e8aa94f0eee6579768179f247de469d15136a
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-02-09 16:32:07 -0300

    Cofiguração do projeto

---
+++ stock-control/deps.md
@@ -0,0 +1,10 @@
+Biblioteca de estilos PrimeNG
+https://www.primefaces.org/primeng-v15-lts/#/
+
+npm install primeng@15.2.1 primeicons@5  primeflex
+
+styles em andular.json
+  "node_modules/primeng/resources/themes/lara-light-blue/theme.css",
+  "node_modules/primeng/resources/primeng.min.css",
+  "node_modules/primeicons/primeicons.css",
+  "node_modules/primeflex/primeflex.css"
--- stock-control/src/app/app.module.ts
+++ stock-control/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
+import { HttpClientModule  } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
