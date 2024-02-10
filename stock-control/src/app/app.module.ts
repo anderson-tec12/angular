4221768babff182a00537dafce30ba4df33a8ca6
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-02-10 18:49:22 -0300

    feat: page home

--- stock-control/src/app/app.module.ts
+++ stock-control/src/app/app.module.ts
@@ -2,21 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 import { HttpClientModule  } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import {CardModule} from 'primeng/card';
+import {ToastModule} from 'primeng/toast'
+import {InputTextModule} from 'primeng/inputtext'
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     HttpClientModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    CardModule,
+    InputTextModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
