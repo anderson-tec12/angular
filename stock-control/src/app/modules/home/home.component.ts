564910b1cdfafbd1ae31f275df77a3c8cd01400d
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-02-11 14:47:26 -0300

    feat: formulario reativo

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,34 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   loginCard = true
 
+
   handler = {
     toggleForm: (value:boolean) => {
       this.loginCard = value
     }
   }
+
+  loginForm = this.formBuilder.group({
+    mail: ['', Validators.required],
+    pass:['', Validators.required]
+  })
+
+  createUserForm = this.formBuilder.group({
+    name:['', Validators.required],
+    mail: ['', [Validators.required, Validators.email]],
+    pass:['', Validators.required]
+  })
+
+  onSubmitLoginForm(event:SubmitEvent):void{
+    console.log(event)
+    console.log(this.loginForm.value)
+  }
+
+  onSubmitCreateUserForm(event:SubmitEvent):void{
+    console.log(event)
+    console.log(this.createUserForm.value)
+  }
+
+  //ReactiveFormModule
+  constructor(private formBuilder:FormBuilder){}
 }
