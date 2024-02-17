6505874d7abd6cf3d822b1397056581126f24fe9
Anderson Barros Silva <anderson.tec12@gmail.com>
2024-02-17 19:36:58 -0300

    feat: modulo de produtos

--- stock-control/src/app/modules/products/pages/products-home/products-home.component.ts
+++ stock-control/src/app/modules/products/pages/products-home/products-home.component.ts
@@ -1,15 +1,68 @@
+import { ProductsDataTransferService } from './../../../../shared/components/services/products/products-data-transfer.service';
 import { CookieService } from 'ngx-cookie-service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { IGetAllProductsResponse } from 'src/app/models/interfaces/products/IGetAllProductsResponse';
+import { MessageService } from 'primeng/api';
 
 @Component({
   templateUrl: './products-home.component.html',
   styleUrls: ['./products-home.component.scss']
 })
-export class ProductsHomeComponent {
-  constructor(
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$ :Subject<void> = new Subject()
+  productsDatas:IGetAllProductsResponse[] = []
 
+  constructor(
+    private productsService:ProductsService,
+    private productsDataTransferService:ProductsDataTransferService,
+    private router:Router,
+    private messageService:MessageService
   ){}
 
 
+  ngOnInit(){
+    this.getServiceProductsData()
+  }
+
+  getServiceProductsData() {
+    const productsLoaded = this.productsDataTransferService.getProductsDatas()
+
+    console.log({productsLoaded})
+
+    if(productsLoaded.length > 0){
+      this.productsDatas = productsLoaded
+    }else{
+      this.getAPIProductsDatas()
+    }
+
+    console.log('DADOS', this.productsDatas)
+  }
+
+  getAPIProductsDatas() {
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next:(response) => {
+          this.productsDatas = response
+        },
+        error: err => {
+          console.log(err)
+          this.messageService.add({
+            severity:'error',
+            summary:'Erro'
+          })
+          this.router.navigate(['/dashboard'])
+
+        }
+      })
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next()
+      this.destroy$.complete()
+  }
 }
